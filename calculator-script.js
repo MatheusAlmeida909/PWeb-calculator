979f91232bc4cad71f6b1fc99390bf462bd2c9e4
Almeida909 <almeidasmatheus909@gmail.com>
2022-09-19 18:56:07 -0300

    Figuring out the functions

---
+++ calculator-script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var leftNum  = parseFloat(document.getElementById("left-num").value);
+    var rightNum = parseFloat(document.getElementById("right-num").value);
+    
+    
+}
