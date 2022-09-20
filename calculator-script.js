7c8d241d5d71bc6da0a83947555fed334aaade91
Almeida909 <almeidasmatheus909@gmail.com>
2022-09-19 21:33:32 -0300

    Finished main structure of calculator ut looking foward to optmize

--- calculator-script.js
+++ calculator-script.js
@@ -1,6 +1,38 @@
-function calculate() {
+function add() {
     var leftNum  = parseFloat(document.getElementById("left-num").value);
     var rightNum = parseFloat(document.getElementById("right-num").value);
-    
-    
+
+    var sum = leftNum + rightNum;
+
+    document.getElementById("result").value = sum;
+}
+
+function subtract() {
+    var leftNum  = parseFloat(document.getElementById("left-num").value);
+    var rightNum = parseFloat(document.getElementById("right-num").value);
+
+    var subtract = leftNum - rightNum;
+
+    document.getElementById("result").value = subtract;
+}
+
+function multiply() {
+    var leftNum  = parseFloat(document.getElementById("left-num").value);
+    var rightNum = parseFloat(document.getElementById("right-num").value);
+
+    var multiply = leftNum * rightNum;
+
+    document.getElementById("result").value = multiply;
+}
+
+function divide() {
+    var leftNum  = parseFloat(document.getElementById("left-num").value);
+    var rightNum = parseFloat(document.getElementById("right-num").value);
+
+    if (!rightNum)
+        alert("Insira um numero que não seja zero para a divisão");
+    else
+        var divide = leftNum / rightNum;
+
+        document.getElementById("result").value = divide;
 }
